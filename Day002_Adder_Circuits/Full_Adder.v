8be7f1f965218d3f9ec87866bc17c90db9253dcc
devanshswaroop01 <153737673+devanshswaroop01@users.noreply.github.com>
2025-09-13 15:27:38 +0530

    Delete Day002_Adder_Circuits/Full_Adder.v

--- Day002_Adder_Circuits/Full_Adder.v
+++
@@ -1,9 +0,0 @@
-//Full Adder 
-module full_adder(a, b, cin, sum, cout);
-  input a, b, cin;
-  output sum, cout;
-assign sum = a ^ b ^ cin;
-  assign cout = (a & b) | (b & cin) | (cin & a);
-endmodule
-
-
