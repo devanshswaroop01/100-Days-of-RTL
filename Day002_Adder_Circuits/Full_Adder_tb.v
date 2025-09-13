31f52b256ee7d445fbc8bc813fc963073e17aff2
devanshswaroop01 <153737673+devanshswaroop01@users.noreply.github.com>
2025-09-13 15:03:05 +0530

    Full_Adder_tb

---
+++ Day002_Adder_Circuits/Full_Adder_tb.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+module full_adder_tb;
+  reg a, b, cin;
+  wire sum, cout;
+  full_adder DUT(a, b, cin, sum, cout);
+
+  initial begin 
+    $dumpfile("waveform.vcd");         
+    $dumpvars(0, full_adder_tb);   
+    $monitor("a = %b, b = %b, cin = %b, sum = %b, cout = %b", a, b, cin, sum, cout); 
+    a = 0; b = 0; cin = 0; #10; 
+    a = 0; b = 0; cin = 1; #10;
+    a = 0; b = 1; cin = 0; #10;
+    a = 0; b = 1; cin = 1; #10;
+    a = 1; b = 0; cin = 0; #10; 
+    a = 1; b = 0; cin = 1; #10;
+    a = 1; b = 1; cin = 0; #10;
+    a = 1; b = 1; cin = 1; #10;
+
+    $finish; 
+  end
+endmodule
+ 
+
+
