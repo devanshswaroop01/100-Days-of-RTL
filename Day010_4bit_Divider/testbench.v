49e09f023d22475720b87bc6023cbbd6a6c04f5b
devanshswaroop01 <153737673+devanshswaroop01@users.noreply.github.com>
2025-09-18 23:10:02 +0530

    Day 010

--- Day010_4bit_Divider/testbench.v
+++ Day010_4bit_Divider/testbench.v
@@ -1,8 +1,8 @@
 //Tesbench:
 `timescale 1ns / 1ps
 module divider_tb;
-    reg  [3:0] dividend, divisor;
-    wire [3:0] quotient, remainder;
+    reg signed  [3:0] dividend, divisor;
+    wire signed [3:0] quotient, remainder;
 divider dut(dividend, divisor, quotient, remainder);
 
 initial begin
@@ -10,7 +10,19 @@ initial begin
     $dumpvars(0, divider_tb);   
 
 $monitor("%0d / %0d = %0d with remainder %0d ",  dividend, divisor, quotient, remainder);
- dividend = 4'b0101; divisor = 4'b1000; #10;
-dividend = 4'b1010; divisor = 4'b0010; #10;
+        dividend =  4'd5;  divisor =  4'd8;  #10;   // 5 / 8 = 0, R=5
+        dividend =  4'd10; divisor =  4'd2;  #10;   // 10 / 2 = 5, R=0
+    
+        dividend =  4'd7;  divisor = -4'd3;  #10;   //  7 / -3 = -2, R=1
+        dividend = -4'd7;  divisor =  4'd3;  #10;   // -7 / 3 = -2, R=-1
+    
+        dividend = -4'd9;  divisor = -4'd3;  #10;   // -9 / -3 = 3, R=0
+        dividend =  4'd5;  divisor =  4'd0;  #10;   // divide by 0
+    
+        dividend =  4'd7;  divisor =  4'd7;  #10;   // 7 / 7 = 1, R=0
+        dividend = -4'd8;  divisor =  4'd1;  #10;   // -8 / 1 = -8, R=0
+
+        #50 $finish;
     end
 endmodule
+
