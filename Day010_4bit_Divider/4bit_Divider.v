09f4250d803be11b6a741553a37956d30a2556be
devanshswaroop01 <153737673+devanshswaroop01@users.noreply.github.com>
2025-09-18 23:06:30 +0530

    Day 010

--- Day010_4bit_Divider/4bit_Divider.v
+++ Day010_4bit_Divider/4bit_Divider.v
@@ -1,30 +1,34 @@
-//4 Bit Divider 
 `timescale 1ns / 1ps
 module divider(
-    input  [3:0] dividend, divisor,
-    output reg [3:0] quotient, remainder
-);
+    input  signed [3:0] dividend, divisor,  
+    output reg signed  [3:0] quotient,      
+    output reg signed  [3:0] remainder );       
+
     always @ (dividend, divisor) begin
         quotient  = 0;
         remainder = dividend;
-        while (remainder >= divisor) begin
-            remainder = remainder - divisor;
-            quotient  = quotient + 1;
+		   //divide by zero
+        if (divisor == 0) begin
+            quotient  = 0;          
+            remainder = dividend;  
+        end 
+
+        else begin
+            //  absolute values 
+            reg [3:0] abs_dividend, abs_divisor, temp_rem;
+            abs_dividend = (dividend < 0) ? -dividend : dividend;
+            abs_divisor  = (divisor  < 0) ? -divisor  : divisor;
+            // iterative subtraction loop 
+            temp_rem = abs_dividend;
+            quotient = 0;
+            while (temp_rem >= abs_divisor) begin
+                temp_rem  = temp_rem - abs_divisor;
+                quotient  = quotient + 1;
+            end
+            // sign correction 
+            if (dividend[3] ^ divisor[3])
+                quotient = -quotient;
+                remainder = (dividend < 0) ? -temp_rem : temp_rem;
+            end
         end
-    end
 endmodule
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
