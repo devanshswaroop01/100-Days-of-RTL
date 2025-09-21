f5cd6d835df9ddebb9f2333848ccf215d041374e
devanshswaroop01 <153737673+devanshswaroop01@users.noreply.github.com>
2025-09-21 19:14:39 +0530

    Day 014

--- Day014_Modelling_Logic_Gates_using_Mux/Basic Logic Gates using Mux/testbench.v
+++ Day014_Modelling_Logic_Gates_using_Mux/Basic Logic Gates using Mux/testbench.v
@@ -2,7 +2,6 @@
 module gates_mux_tb;
     reg a, b;
     wire and_out, or_out, not_out;
-
     gates_mux dut(
         .a(a), .b(b),
         .and_out(and_out),
@@ -11,12 +10,13 @@ module gates_mux_tb;
  initial begin
         $dumpfile("waveform.vcd");
         $dumpvars(0, gates_mux_tb);
-$monitor("Time=%0t | a=%b b=%b | AND=%b OR=%b NOT(a)=%b",
-$time, a, b, and_out, or_out, not_out);
-        a = 0; b = 0;
+        $monitor("Time=%0t | a=%b b=%b | AND=%b OR=%b NOT(a)=%b",$time, a, b, and_out, or_out, not_out);
+        
+         a = 0; b = 0;
         #10 a = 0; b = 1;
         #10 a = 1; b = 0;
         #10 a = 1; b = 1;
         #10 $finish;
     end
 endmodule
+
