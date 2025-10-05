41f4a11ec9bd565c2955d06da161c8bec2e35edc
devanshswaroop01 <153737673+devanshswaroop01@users.noreply.github.com>
2025-10-05 10:44:32 +0530

    Day 026

--- Day_026_Binary_to_Gray_code_Converter/testbench.v
+++ Day_026_Binary_to_Gray_code_Converter/testbench.v
@@ -1,26 +1,4 @@
-module gray_to_binary_tb;
-reg  [3:0] gray_in;      
- wire [3:0] binary_out;    
-gray_to_binary uut (
-        .gray_in(gray_in),
-        .binary_out(binary_out));
-
-    initial begin
-        $dumpfile("waveform.vcd");         
-        $dumpvars(0, gray_to_binary_tb);   
-        $display("Time\tGray\tBinary");
-        $monitor("%0dns\t%b\t%b", $time, gray_in, binary_out);
-      
-        gray_in = 4'b0011; #10;
-        gray_in = 4'b0010; #10;
-       gray_in = 4'b0101; #10;
-        gray_in = 4'b0100; #10;
-       
-$finish;
-end
-endmodule
-
-//Testbench
+//testbench
 module binary_to_gray_tb;
 reg [3:0] bin;
 wire [3:0] gray;
@@ -40,6 +18,7 @@ initial begin
 end
 endmodule
  
+
 
 
 
