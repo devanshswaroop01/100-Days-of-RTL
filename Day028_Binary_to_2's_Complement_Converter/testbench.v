4d9c08918f4161e3164fec176058370b780a04d6
devanshswaroop01 <153737673+devanshswaroop01@users.noreply.github.com>
2025-10-07 18:25:57 +0530

    Day 028

--- Day028_Binary_to_2's_Complement_Converter/testbench.v
+++ Day028_Binary_to_2's_Complement_Converter/testbench.v
@@ -1,35 +1,29 @@
-`timescale 1ns / 1ps
-
-module test_bench;
-  reg  [3:0] data;          // unsigned 4-bit input
-  wire signed [3:0] out;    // signed 4-bit output (2's complement)
-
-  // DUT instantiation
-  complement_2s dut (
-    .data(data),
-    .out(out)
-  );
+`timescale 1ns / 1ps\
+module complment_2s_tb;
+  reg  [3:0] data;         
+  wire signed [3:0] out;   
+  complement_2s dut ( .data(data), .out(out));
 
   initial begin
     $dumpfile("waveform.vcd");
-    $dumpvars(0, test_bench);
-
-    // Print input and output in both decimal and binary
-    $monitor("Time=%0t | Input=%0d (%b) -> 2's Complement=%0d (%b)", 
-              $time, data, data, out, out);
+    $dumpvars(0,complment_2s_tb);
+    $monitor("Time=%0t | Input=%0d (%b) -> 2's Complement=%0d (%b)",  $time, data, data, out, out);
 
     data = 4'd0;  #10;
     data = 4'd1;  #10;
     data = 4'd2;  #10;
     data = 4'd3;  #10;
+    
     data = 4'd4;  #10;
     data = 4'd5;  #10;
     data = 4'd6;  #10;
     data = 4'd7;  #10;
+    
     data = 4'd8;  #10;
     data = 4'd9;  #10;
     data = 4'd10; #10;
     data = 4'd11; #10;
+    
     data = 4'd12; #10;
     data = 4'd13; #10;
     data = 4'd14; #10;
@@ -38,3 +32,4 @@ module test_bench;
     #10 $finish;
   end
 endmodule
+
