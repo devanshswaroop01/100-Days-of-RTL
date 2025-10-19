077b8deeb75f004945ba054473d7765b5a1a4881
devanshswaroop01 <153737673+devanshswaroop01@users.noreply.github.com>
2025-10-19 22:12:39 +0530

    Day 040

--- Day040_T_Flipflop/testbench.v
+++ Day040_T_Flipflop/testbench.v
@@ -12,15 +12,24 @@ module t_FF_tb;
     $dumpfile("waveform.vcd");
     $dumpvars(0, t_FF_tb); 
     $monitor("Time=%0t | t=%b reset=%b | q=%b", $time, t, reset, q);
-   reset = 0; t = 0; #10;  
+    
+    reset = 0; t = 0; #10; 
+    
     reset = 1; t = 1; #10;   // Reset active
+    
     reset = 0; t = 1; #20;   // Toggle twice
+    
     t = 0; #10;              // Hold
+    
     t = 1; #10;              // Toggle
+    
     reset = 1; #5;           // Async reset mid-cycle
+    
     reset = 0; t = 1; #10;   // Resume toggling
+    
     t = 0; #10;              // Hold
 
     $finish;
   end
 endmodule
+
