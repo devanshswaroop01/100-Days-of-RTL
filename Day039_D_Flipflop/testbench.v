45075ae934378a848ed44943f2ed86aff971b246
devanshswaroop01 <153737673+devanshswaroop01@users.noreply.github.com>
2025-10-19 22:18:10 +0530

    Day 039

--- Day039_D_Flipflop/testbench.v
+++ Day039_D_Flipflop/testbench.v
@@ -14,14 +14,17 @@ module d_FF_tb;
     $dumpvars(0, d_FF_tb); 
     $monitor("Time=%0t | d=%b reset=%b | q=%b", $time, d, reset, q);
 
-    reset = 0; d = 0; #10;
-    d = 1; #10;
-    d = 0; #10;
-    reset = 1; #5;
-    reset = 0; #5;
-    d = 1; #10;
-    d = 0; #10;
+    reset = 0; d = 0; #10;   // Deactivate reset
+    d = 1; #10;              // Apply D = 1
+    d = 0; #10;              // Apply D = 1
+    
+    reset = 1; #5;           //  Activate reset
+    
+    reset = 0; #5;           // Deactivate reset
+    d = 1; #10;              // Apply D = 1 
+    d = 0; #10;              // Apply D = 0
 
     $finish;
   end
 endmodule
+
