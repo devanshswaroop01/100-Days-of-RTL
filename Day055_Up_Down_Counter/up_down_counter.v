52ad9083e7ed6bc1818ae7d23d3637c65247adc6
devanshswaroop01 <153737673+devanshswaroop01@users.noreply.github.com>
2025-11-03 10:51:08 +0530

    Day 055

--- Day055_Up_Down_Counter/up_down_counter.v
+++ Day055_Up_Down_Counter/up_down_counter.v
@@ -1,3 +1,4 @@
+// Up Down Counter
 `timescale 1ns / 1ps
 module up_down_counter
 (clk, reset, upordown, count);
@@ -10,20 +11,19 @@ module up_down_counter
         count <= 0;
 
         else if (upordown == 1)     // Up mode
-
             if (count == 2*N - 1)
                 count <= 0;         // wrap around
             else
                 count <= count + 1; // increment counter
 
         else                        // Down mode
-
             if (count == 0)
                 count <= 2*N - 1;   // wrap around
             else
                 count <= count - 1; // decrement counter
     end
 endmodule
+
 
 
 
