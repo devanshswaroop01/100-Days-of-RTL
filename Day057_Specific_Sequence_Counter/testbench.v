10ce84cdf6e28155c723d068b2e8c44fd1b35c5d
devanshswaroop01 <153737673+devanshswaroop01@users.noreply.github.com>
2025-11-05 15:48:19 +0530

    Day 057

---
+++ Day057_Specific_Sequence_Counter/testbench.v
@@ -0,0 +1,19 @@
+module testbench;
+reg clk, reset;
+wire [3:0] counter;
+sequence_counter dut(clk, reset,counter);
+
+initial  begin
+        clk = 0;
+        reset = 1;
+        # 10 reset = 0 ;
+        end
+always #5 clk = ~clk;
+
+    initial begin
+    $dumpfile("waveform.vcd");
+    $dumpvars(0, testbench);
+    $monitor("\t\t clk: %b    counter:%b", clk, counter);
+    #140 $finish;
+    end
+endmodule
