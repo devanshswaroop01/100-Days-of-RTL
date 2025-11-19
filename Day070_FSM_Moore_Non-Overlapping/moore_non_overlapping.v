883e0e60ea97468ee6c281e4e1e6797de2835287
devanshswaroop01 <153737673+devanshswaroop01@users.noreply.github.com>
2025-11-19 11:45:17 +0530

    Day 070

---
+++ Day070_FSM_Moore_Non-Overlapping/moore_non_overlapping.v
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+module fsm_1011(
+    input clk,rst,din,
+    output reg y);
+// state encoding 
+  parameter S0 = 3'b000,
+            S1 = 3'b001,
+            S2 = 3'b010,
+            S3 = 3'b011,
+            S4 = 3'b100;
+reg [2:0] cs, nst;
+
+  // state transition logic (sequential)
+  always @(posedge clk, posedge rst) begin
+    if (rst)
+      cs <= S0;
+    else
+      cs <= nst;
+  end
+
+  // next state logic (combinational)
+  always @(*) begin
+    case (cs)
+      S0: nst = (din) ? S1 : S0;
+      S1: nst = (din) ? S1 : S2;
+      S2: nst = (din) ? S3 : S0;
+      S3: nst = (din) ? S4 : S0;
+      S4: nst = (din) ? S1 : S0;
+      default: nst = S0;
+    endcase
+  end
+
+  //output logic (combinational) 
+always @(cs) begin
+	case(cs)
+    S0: y<=0;
+    S1: y<=0;
+    S2: y<=0;
+    S3: y<=0;
+    S4: y<=1;
+default: y<=0;
+  endcase
+    end
+  endmodule 
