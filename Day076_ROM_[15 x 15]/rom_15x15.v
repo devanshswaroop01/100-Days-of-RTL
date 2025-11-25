b0687d1382da2ce8c4c5ea7402ae050bcd17e85b
devanshswaroop01 <153737673+devanshswaroop01@users.noreply.github.com>
2025-11-25 11:08:03 +0530

    Day 076

---
+++ Day076_ROM_[15 x 15]/rom_15x15.v
@@ -0,0 +1,15 @@
+//15 x 15 rom
+`timescale 1ns / 1ps
+module rom(
+    input clk, r_en,
+    input [3:0] addr,
+    output reg [15:0] data);
+    reg [15:0] mem [15:0];
+
+    always@(posedge clk) begin
+        if(r_en)
+            data <= mem[addr];
+        else
+            data <= 'bz;
+    end
+endmodule
