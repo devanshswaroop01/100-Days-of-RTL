95ec9357cb45c89cb5ba3c6c734e76495f28b208
devanshswaroop01 <153737673+devanshswaroop01@users.noreply.github.com>
2025-11-29 03:31:41 +0530

    Day 080

---
+++ Day080_Checking if Number is a Prime Number/prime_number.v
@@ -0,0 +1,38 @@
+module prime(
+    input  [2:0] number,
+    output reg   is_prime );
+    localparam PRIME     = 1'b1,
+               NOT_PRIME = 1'b0;
+
+    always @(number) begin
+        is_prime = check_prime(number);
+
+        if (is_prime)
+            $display("%d is a prime number", number);
+        else
+            $display("%d is NOT a prime number", number);
+    end
+
+    function automatic check_prime;
+        input [2:0] num;
+        integer i;
+        begin
+            // handle small non-primes
+            if (num < 2) begin
+                check_prime = NOT_PRIME;
+            end
+            else begin
+                check_prime = PRIME;  // assume prime
+
+                for (i = 2; i <= num/2; i = i + 1) begin
+                    if (num % i == 0)
+                        check_prime = NOT_PRIME;
+                end
+                end
+            end
+          endfunction
+      endmodule
+
+
+
+ 
