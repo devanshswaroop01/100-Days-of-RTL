b7db0d146d4cb660757d07116677a653bb958089
devanshswaroop01 <153737673+devanshswaroop01@users.noreply.github.com>
2025-12-01 19:46:35 +0530

    Day 082

--- Day082_Factorial_of.a_Number/testbench.v
+++ Day082_Factorial_of.a_Number/testbench.v
@@ -7,12 +7,11 @@ integer x;
 factorial dut(n, result);
 
 always for(x=0; x<11; x=x+1) #10 n=x;
-
 initial begin 
    $dumpfile("waveform.vcd");
    $dumpvars(0, factorial_tb);
-
-$monitor("\t\t factorial of %d is %0d",n, result);
+   $monitor("\t\t factorial of %d is %0d",n, result);
 #120 $finish;
 end
 endmodule
+
