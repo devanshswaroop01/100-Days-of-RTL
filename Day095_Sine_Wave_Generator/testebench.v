9b2919d9831b4c3c24eee5884f459aef7552d4a9
devanshswaroop01 <153737673+devanshswaroop01@users.noreply.github.com>
2025-12-17 20:02:06 +0530

    Day 095

---
+++ Day095_Sine_Wave_Generator/testebench.v
@@ -0,0 +1,21 @@
+// Code your testbench here
+// or browse Examples
+`timescale 1ns / 1ps
+
+module testbench_sinewave;
+reg [9:0]t;
+wire [15:0]o;
+sine_wave_generator SWG(o,t);
+integer i;
+initial
+begin
+  $dumpfile("waveform.vcd");
+  $dumpvars(0, testbench_sinewave);
+$monitor("o=%d",o);
+    #5 t=0;
+     for(i=0;i<1024;i=i+1)
+    begin
+        #5 t=i;
+    end
+end
+endmodule
