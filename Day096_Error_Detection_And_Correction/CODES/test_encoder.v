a1fc769e3ac13684a16db0fe27dfff3f6500ccca
devanshswaroop01 <153737673+devanshswaroop01@users.noreply.github.com>
2025-12-18 22:36:49 +0530

    Day 096

---
+++ Day096_Error_Detection_And_Correction/CODES/test_encoder.v
@@ -0,0 +1,16 @@
+`timescale 1ns / 1ps
+
+module test_encoder;
+    reg [3:0]data;
+    wire [6:0] ham_data_encoder;
+    
+    hamming_encoder dut(data, ham_data_encoder);
+    
+    initial begin
+       $dumpfile("waveform.vcd");
+      $dumpvars(0, test_encoder);
+     data=4'b0110;	
+     #2 $display("\n \t Output of Hamming Encoder: %b \n",ham_data_encoder);
+    #10 $stop;  //we will get output=1010101, please check in console after run the simulation
+    end
+    endmodule
