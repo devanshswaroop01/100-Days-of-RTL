a686eca211c1695f4e7ef0e0b194fdc2cea60955
devanshswaroop01 <153737673+devanshswaroop01@users.noreply.github.com>
2025-12-22 00:12:47 +0530

    Day 100

--- Day100_Serial_Port_Interface_(SPI)_Protocol/testbench.v
+++ Day100_Serial_Port_Interface_(SPI)_Protocol/testbench.v
@@ -1,7 +1,5 @@
 `timescale 1ns / 1ps
-
 module test_bench;
-
     reg         clk;
     reg         reset;
     reg [15:0]  datain;
@@ -10,18 +8,12 @@ module test_bench;
     wire        spi_sclk;
     wire        spi_data;
     wire [4:0]  counter;
-
     integer frame_count;
-
-    // DUT
     spi_state dut ( clk, reset, datain, spi_cs_l, spi_sclk, spi_data, counter );
 
-    // ---------------- Clock Generation ----------------
     initial clk = 0;
-    always #5 clk = ~clk;
-
-    // ---------------- Frame Counter ----------------
-    initial frame_count = 0;
+    always #5 clk = ~clk;  //  Clock Generation 
+    initial frame_count = 0; //  Frame Counter 
 
     // ---------------- SPI Frame Completion Monitor ----------------
     always @(posedge spi_cs_l) begin
@@ -36,7 +28,6 @@ module test_bench;
     end
 
     // OPTIONAL: Bit-level monitor (enable if needed)
-
     always @(posedge spi_sclk) begin
         if (spi_cs_l == 0)
             $display("Time=%0t | SCLK↑ | MOSI=%b | BitCount=%0d",
@@ -51,7 +42,6 @@ module test_bench;
 
         reset  = 1'b1;
         datain = 16'h0000;
-
         #30;
         reset = 1'b0;
 
@@ -60,25 +50,22 @@ module test_bench;
         #10 datain = 16'h0412;
         wait (spi_cs_l == 1'b0);
         wait (spi_cs_l == 1'b1);
-
         #40;
 
         // -------- FRAME 2 --------
         datain = 16'h4839;
         wait (spi_cs_l == 1'b0);
         wait (spi_cs_l == 1'b1);
-
         #40;
 
         // -------- FRAME 3 --------
         datain = 16'hABEB;
         wait (spi_cs_l == 1'b0);
         wait (spi_cs_l == 1'b1);
-
         #50;
-
         $display("All SPI frames completed successfully at time %0t ns", $time);
         $finish;
     end
 
 endmodule
+
